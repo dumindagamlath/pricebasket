9ceb690e150eb42f0dc2356d24b62ae2205993da
802213 <duminda.gamlath@hotelbeds.com>
2020-10-14 16:25:13 +0100

    Refactored comments etc.

--- src/main/java/com/store/exception/ProductNotFoundException.java
+++ src/main/java/com/store/exception/ProductNotFoundException.java
@@ -1,7 +1,13 @@
 package com.store.exception;
 
+/**
+ * Exception when product is not found.
+ */
 public class ProductNotFoundException extends RuntimeException{
-
+    /**
+     * Product not found
+     * @param message detail message with the product
+     */
     public ProductNotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/store/model/Order.java
+++ src/main/java/com/store/model/Order.java
@@ -12,7 +12,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
